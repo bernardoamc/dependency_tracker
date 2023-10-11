21bf3968d3aa59e58bf85edb27ce789f7a368db3
Bernardo de Araujo <bernardo.araujo@shopify.com>
2023-10-10 23:25:11 -0400

    Add methods to detect dependecy issues for Ruby and JS

--- lib/dependency_tracker.ex
+++ lib/dependency_tracker.ex
@@ -1,6 +1,6 @@
 defmodule DependencyTracker do
-  alias DependencyTracker.Specification
-  alias DependencyTracker.Ruby.PackageDefinition
+  alias DependencyTracker.Ruby
+  alias DependencyTracker.Javascript
 
   @moduledoc """
   This module is the entry point to the application. It provides a single function
@@ -9,7 +9,7 @@ defmodule DependencyTracker do
   """
 
   @doc """
-  Given a Specification struct and a PackageDefinition struct, returns a list of
+  Given a Ruby.Specification struct and a Ruby.PackageDefinition struct, returns a list of
   issues. An issue is detected when the remote URL of a dependency in the Specification
   struct does not match the URL of the same dependency in the PackageDefinition struct.
 
@@ -24,35 +24,85 @@ defmodule DependencyTracker do
 
   ## Examples
       iex> {:ok, package_definition} = DependencyTracker.Ruby.PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
-      iex> specification = DependencyTracker.Specification.new(["aasm"], "https://acme.io/basic/gems/ruby/")
-      iex> DependencyTracker.issues(specification, package_definition)
+      iex> specification = DependencyTracker.Ruby.Specification.new(["aasm"], "https://acme.io/basic/gems/ruby/")
+      iex> DependencyTracker.detect_ruby_issues(specification, package_definition)
       [
         %{
-          specification_url: "https://acme.io/basic/gems/ruby/",
           dependency: "aasm",
-          package_definition_url: "https://rubygems.org/"
+          expected_url: "https://acme.io/basic/gems/ruby/",
+          request_url: "https://rubygems.org/"
         }
       ]
   """
-  def issues(specification, package_definition) do
-    PackageDefinition.remote_urls(package_definition)
+  def detect_ruby_issues(%Ruby.Specification{constraints: _ } = specification, package_definition) do
+    Ruby.PackageDefinition.remote_urls(package_definition)
     |> Enum.reduce([], fn remote_url, acc ->
-      {:ok, gems} = PackageDefinition.packages(package_definition, remote_url)
+      {:ok, gems} = Ruby.PackageDefinition.packages(package_definition, remote_url)
 
       Enum.reduce(gems, acc, fn gem, acc ->
-        case Specification.valid_dependency?(specification, gem, remote_url) do
+        case Ruby.Specification.valid_dependency?(specification, remote_url, gem) do
           {:ok, _} -> acc
-          {:error, expected_url} -> [create_issue(gem, remote_url, expected_url) | acc]
+          {:error, expected_url} -> [create_ruby_issue(gem, expected_url, remote_url) | acc]
         end
       end)
     end)
   end
 
-  defp create_issue(gem, remote_url, specification_url) do
+
+  @doc """
+  Given a Javascript.Specification struct and a Javascript.PackageDefinition struct,
+  returns a list of issues. An issue is detected when the organization of a dependency
+  in the Specification struct does not match the organization of the same dependency in
+  the PackageDefinition struct.
+
+  If the organization of a dependency in the Specification struct does not exist in
+  the PackageDefinition struct, it is ignored.
+
+  When an issue is detected, a map is returned containing the dependency name,
+  the organization of the dependency in the PackageDefinition struct and the organizaion
+  of the dependency in the Specification struct.
+
+  Returns a list of issues or an empty list.
+
+  ## Examples
+      iex> {:ok, package_definition} = DependencyTracker.Javascript.PackageDefinition.parse("test/fixtures/javascript/yarn.lock")
+      iex> specification = DependencyTracker.Javascript.Specification.new("babel", ["yallist"])
+      iex> DependencyTracker.detect_javascript_issues(specification, package_definition)
+      [
+        %{
+          dependency: "yallist",
+          expected_org: "babel",
+          requested_org: "public_npm"
+        }
+      ]
+  """
+  def detect_javascript_issues(%Javascript.Specification{constraints: _ } = specification, package_definition) do
+    Javascript.PackageDefinition.orgs(package_definition)
+    |> Enum.reduce([], fn org, acc ->
+      {:ok, packages} = Javascript.PackageDefinition.packages(package_definition, org)
+
+      Enum.reduce(packages, acc, fn package, acc ->
+        case Javascript.Specification.valid_dependency?(specification, org, package.name) do
+          {:ok, _} -> acc
+          {:error, expected_org} -> [create_javascript_issue(package.name, expected_org, org) | acc]
+        end
+      end)
+    end)
+  end
+
+  defp create_ruby_issue(gem, expected_url, requested_url) do
     %{
       dependency: gem,
-      package_definition_url: remote_url,
-      specification_url: specification_url
+      expected_url: expected_url,
+      request_url: requested_url
+    }
+  end
+
+  defp create_javascript_issue(package, expected_org, requested_org) do
+    %{
+      dependency: package,
+      expected_org: expected_org,
+      requested_org: requested_org
     }
   end
 end
--- test/dependency_tracker_test.exs
+++ test/dependency_tracker_test.exs
@@ -2,34 +2,64 @@ defmodule DependencyTrackerTest do
   use ExUnit.Case
   doctest DependencyTracker
 
-  alias DependencyTracker.Ruby.PackageDefinition
+  alias DependencyTracker.Ruby
+  alias DependencyTracker.Javascript
 
-  test "detects issues between Specification and PackageDefinition" do
-    {:ok, package_definition} = PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
-    specification = DependencyTracker.Specification.new(["aasm"], "https://acme.io/basic/gems/ruby/")
+  test "detect_ruby_issues returns the list of issues between Specification and PackageDefinition" do
+    {:ok, package_definition} = Ruby.PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
+    specification = Ruby.Specification.new(["aasm"], "https://acme.io/basic/gems/ruby/")
 
     expected_issues = [
       %{
-        specification_url: "https://acme.io/basic/gems/ruby/",
         dependency: "aasm",
-        package_definition_url: "https://rubygems.org/"
+        expected_url: "https://acme.io/basic/gems/ruby/",
+        request_url: "https://rubygems.org/"
       }
     ]
 
-    assert DependencyTracker.issues(specification, package_definition) == expected_issues
+    assert DependencyTracker.detect_ruby_issues(specification, package_definition) == expected_issues
   end
 
-  test "returns empty list of issues when Specification is empty" do
-    {:ok, package_definition} = PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
-    specification = DependencyTracker.Specification.new()
+  test "detect_ruby_issues returns empty list of issues when Specification is empty" do
+    {:ok, package_definition} = Ruby.PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
+    specification = Ruby.Specification.new()
 
-    assert DependencyTracker.issues(specification, package_definition) == []
+    assert DependencyTracker.detect_ruby_issues(specification, package_definition) == []
   end
 
-  test "returns empty list of issues when specifications are met" do
-    {:ok, package_definition} = PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
-    specification = DependencyTracker.Specification.new(["aasm"], "https://rubygems.org/")
+  test "detect_ruby_issues returns empty list of issues when specifications are met" do
+    {:ok, package_definition} = Ruby.PackageDefinition.parse("test/fixtures/ruby/Gemfile.lock")
+    specification = Ruby.Specification.new(["aasm"], "https://rubygems.org/")
 
-    assert DependencyTracker.issues(specification, package_definition) == []
+    assert DependencyTracker.detect_ruby_issues(specification, package_definition) == []
+  end
+
+  test "detect_javascript_issues returns the list of issues between Specification and PackageDefinition" do
+    {:ok, package_definition} = Javascript.PackageDefinition.parse("test/fixtures/javascript/yarn.lock")
+    specification = Javascript.Specification.new("acme", ["compat-data"])
+
+    expected_issues = [
+      %{
+        dependency: "compat-data",
+        expected_org: "acme",
+        requested_org: "babel"
+      }
+    ]
+
+    assert DependencyTracker.detect_javascript_issues(specification, package_definition) == expected_issues
+  end
+
+  test "detect_javascript_issues returns empty list of issues when Specification is empty" do
+    {:ok, package_definition} = Javascript.PackageDefinition.parse("test/fixtures/javascript/yarn.lock")
+    specification = Javascript.Specification.new()
+
+    assert DependencyTracker.detect_javascript_issues(specification, package_definition) == []
+  end
+
+  test "detect_javascript_issues returns empty list of issues when specifications are met" do
+    {:ok, package_definition} = Javascript.PackageDefinition.parse("test/fixtures/javascript/yarn.lock")
+    specification = Javascript.Specification.new("babel", ["compat-data"])
+
+    assert DependencyTracker.detect_javascript_issues(specification, package_definition) == []
   end
 end
